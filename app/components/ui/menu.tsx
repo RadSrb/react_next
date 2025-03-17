a54808b068f0de6000737ec7d3daf03997cf9f33
RadSrb <alex.radic@gmx.at>
2025-03-17 10:41:29 +0100

    menu

--- app/components/ui/menu.tsx
+++ app/components/ui/menu.tsx
@@ -7,13 +7,13 @@ export default function Menu({ }: Props) {
     return (
         <div>
             <ul className='border-2 border-gray-400 rounded-xl m-4 shadow flex p-4'>
-                <li className='p-2 hover:bg-amber-200'>
+                <li className='p-4 hover:bg-amber-200'>
                     <Link href="/">Home</Link>
                 </li>
-                <li className='p-2 hover:bg-amber-200'>
+                <li className='p-4 hover:bg-amber-200'>
                     <Link href="/cars">Cars</Link>
                 </li>
-                <li className='p-2 hover:bg-amber-200'>
+                <li className='p-4 hover:bg-amber-200'>
                     <Link href="/trucks">Trucks</Link>
                 </li>
             </ul>
